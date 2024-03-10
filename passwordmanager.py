d86e5703947e30d7737c1f191195c90d7d322bdf
Zanadukhan <cchiu231998@hotmail.ca>
2024-03-09 20:17:49 -0800

    added encryption and decryption when adding new entries and editing them

--- passwordmanager.py
+++ passwordmanager.py
@@ -2,6 +2,7 @@
 from getpass import getpass
 import os
 import sys
+import base64
 import mysql.connector
 
 def editdb(query:str):
@@ -14,9 +15,15 @@ def editdb(query:str):
     mydb.commit()
     print('')
 
+def passwordencryption(pwd):
+    return base64.b64encode(pwd.encode('utf-8'))
+
+def decodepassword(encrypted):
+    return base64.b64decode(encrypted).decode('utf-8')
+    
 # TODO: Add Salting and Hashing Function to every password occurance
 # TODO: Initial DB creation for first time use
-# TODO: Add a new function for getting unhashed and desalted password or figure out how to return it
+# TODO: Add a new function for getting unhashed and desalted password or figure out how to return
 mydb = mysql.connector.connect(
         host ='localhost',
         user ='root',
@@ -44,23 +51,30 @@ while True:
             myresults = mycursor.fetchall()
             print('')
             for result in myresults:
+                result = list(result)
+                decrypted = decodepassword(result[3])
+                result[3] = decrypted
                 print(result)
         case 2:
             # new entry is added by taking in user entries and creating a new mysql tuple by inserting info into logininfo table
             app = input('What is the name of the application? ')
             username = input('Enter your username: ')
             password = getpass('Enter your password: ')
+            # TODO: create Password generator
+            encryptpass = passwordencryption(password)
             SQL = 'INSERT INTO logininfo (Application, Username, Password) VALUES (%s, %s, %s)'
-            VAL = (app, username, password)
+            VAL = (app, username, encryptpass)
             mycursor.execute(SQL, VAL)
             mydb.commit()
             print(f'login info for {app} has been added')
         case 3:
             app = input('What application are you trying to change? ')
+            # TODO: check if application exists
             choice = input('Are you editing the password or username? ')
             if choice == 'password':
                 # this section allows the user to change the current password for chosen app
                 new_pass = getpass('what is your new password? ')
+                new_pass = passwordencryption(new_pass)
                 edit = f"UPDATE logininfo SET Password = '{new_pass}' WHERE Application = '{app}'"
                 editdb(edit)
                 print(f'{app} has been updated with a new password')
